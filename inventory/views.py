99b389f9914b3db1879816457d661cd068bd2ab5
B3immarr <brett222wambugu@gmail.com>
2025-05-30 19:36:16 +0300

    simpler views

--- inventory/views.py
+++ inventory/views.py
@@ -4,22 +4,9 @@ from .models import Item, Category, Brand
 
 
 # inventory/views.py
+from django.shortcuts import render
+from .models import Item
 
 def item_list(request):
-    category = request.GET.get('category')
-    brand = request.GET.get('brand')
-
-    items = Item.objects.all()
-    if category:
-        items = items.filter(category=category)
-    if brand:
-        items = items.filter(brand=brand)
-
-    categories = Item.objects.values_list('category', flat=True).distinct()
-    brands = Item.objects.values_list('brand', flat=True).distinct()
-
-    return render(request, 'item_list.html', {
-        'items': items,
-        'categories': categories,
-        'brands': brands,
-    })
+    items = Item.objects.all()  # Get all items from the database
+    return render(request, 'item_list.html', {'items': items})
