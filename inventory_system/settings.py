8ec716310e60400abd72ed81665a0e22dc6a029d
B3immarr <brett222wambugu@gmail.com>
2025-05-30 20:33:26 +0300

    Switched to Postgresql

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path,os
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -88,11 +88,9 @@ WSGI_APPLICATION = 'inventory_system.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
-}
+
 
 
 # Password validation
